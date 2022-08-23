5c263222b14f19c8723a0d74f93b193974f628f0
amounach42 <mounach-@hotmail.com>
2022-08-23 01:48:59 +0100

    chore: add get and post methods to BookController

---
+++ Controllers/BookController.cs
@@ -0,0 +1,49 @@
+using  Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers;
+
+[ApiController]
+[Route("Api/[Controller]")]
+public class BookController : ControllerBase
+{
+    private static List<Book> books = new List<Book>
+        {
+            new Book{
+                Id = 0,
+                Title = "Book 1",
+                AuthorName = "Author 1",
+                PublishDate = new DateTime(2011, 08, 11),
+                ReviewDate = new DateTime(2013, 01, 10),         
+                Price = 300,
+            },
+            new Book{
+                Id = 1,
+                Title = "Book 2",
+                AuthorName = "Author 2",
+                PublishDate = new DateTime(2010, 04, 01),
+                ReviewDate = new DateTime(2011, 01, 10),         
+                Price = 250,
+            }
+
+        };
+    [HttpGet]
+    public async Task<ActionResult<List<Book>>> Get() 
+    {
+        return Ok(books);
+    }
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<Book>> Get(int id)
+    {
+        var book = books.Find(b => b.Id == id);
+        if (book == null)
+            return BadRequest("Book Not found.");
+        return Ok(book);
+    }
+    [HttpPost]
+    public async Task<ActionResult<Book>> Post(Book book)
+    {
+        books.Add(book);
+        return Ok(book);
+    }
+}
